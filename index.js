da3bcffb0bcd39213b9c49cf66f3736e32c5aed2
Jon Schlinkert <jon.schlinkert@sellside.com>
2018-12-13 23:11:09 -0500

    refactor

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 'use strict';
 
-var isObject = require('is-extendable');
-var union = require('arr-union');
-var get = require('get-value');
-var set = require('set-value');
+const get = require('get-value');
+const set = require('set-value');
 
-module.exports = function unionValue(obj, prop, value) {
+const isObject = val => {
+  return val != null && typeof val === 'object' && !Array.isArray(val);
+};
+
+const flatten = (...args) => {
+  let res = [];
+  let flat = arr => {
+    for (let ele of arr) Array.isArray(ele) ? flat(ele, res) : res.push(ele);
+  };
+  flat(args);
+  return res;
+};
+
+const unique = arr => arr.filter((v, i) => arr.indexOf(v) === i);
+const union = (...args) => unique(flatten(...args));
+const first = (...args) => args.find(v => v != null);
+
+module.exports = (obj, prop, value) => {
   if (!isObject(obj)) {
-    throw new TypeError('union-value expects the first argument to be an object.');
+    throw new TypeError('expected the first argument to be an object');
   }
 
   if (typeof prop !== 'string') {
-    throw new TypeError('union-value expects `prop` to be a string.');
+    throw new TypeError('expected the second argument to be a string');
   }
 
-  var arr = arrayify(get(obj, prop));
-  set(obj, prop, union(arr, arrayify(value)));
+  let arr = [].concat(first(get(obj, prop), []));
+  set(obj, prop, union(arr, [].concat(first(value, []))));
   return obj;
 };
-
-function arrayify(val) {
-  if (val === null || typeof val === 'undefined') {
-    return [];
-  }
-  if (Array.isArray(val)) {
-    return val;
-  }
-  return [val];
-}
--- test.js
+++ test.js
@@ -1,51 +1,38 @@
-/*!
- * union-value <https://github.com/jonschlinkert/union-value>
- *
- * Copyright (c) 2015 .
- * Licensed under the MIT license.
- */
-
 'use strict';
 
 require('mocha');
-require('should');
-var assert = require('assert');
-var union = require('./');
+const assert = require('assert');
+const union = require('./');
 
-describe('union', function() {
-  it('should add a value:', function() {
-    var obj = {};
+describe('union', () => {
+  it('should add a value:', () => {
+    let obj = {};
     union(obj, 'foo', ['a', 'b'])
-    obj.foo.should.eql(['a', 'b']);
+    assert.deepEqual(obj.foo, ['a', 'b']);
   });
 
-  it('should union a value:', function() {
-    var obj = {foo: ['a', 'b']};
+  it('should union a value:', () => {
+    let obj = {foo: ['a', 'b']};
     union(obj, 'foo', ['c', 'd', 'e'])
-    obj.foo.should.eql(['a', 'b', 'c', 'd', 'e']);
+    assert.deepEqual(obj.foo, ['a', 'b', 'c', 'd', 'e']);
   });
 
-  it('should union a deeply nested value:', function() {
-    var obj = {};
+  it('should union a deeply nested value:', () => {
+    let obj = {};
     union(obj, 'a.b.c', ['one', 'two']);
     union(obj, 'a.b.c', ['three']);
-    obj.a.b.c.should.eql(['one', 'two', 'three']);
+    assert.deepEqual(obj.a.b.c, ['one', 'two', 'three']);
   });
 
-  it('should support escaped dots:', function() {
-    var obj = {};
+  it('should support escaped dots:', () => {
+    let obj = {};
     union(obj, 'a\\.b.c', ['one', 'two']);
     union(obj, 'a\\.b.c', ['three']);
-    obj['a.b'].c.should.eql(['one', 'two', 'three']);
+    assert.deepEqual(obj['a.b'].c, ['one', 'two', 'three']);
   });
 
-  it('should throw an error:', function() {
-    (function() {
-      union();
-    }).should.throw('union-value expects the first argument to be an object.');
-
-    (function() {
-      union({});
-    }).should.throw('union-value expects `prop` to be a string.');
+  it('should throw an error:', () => {
+    assert.throws(() => union(), /expected the first argument to be an object/);
+    assert.throws(() => union({}), /expected the second argument to be a string/);
   });
 });
